f90a1b7cc181b0edc62e0be28ca824ba4c445454
Cameron Jones <iamcrjones@gmail.com>
2022-02-03 11:55:19 +1000

    movie form

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,4 +7,17 @@ class MoviesController < ApplicationController
     def show
         @movie = Movie.find(params[:id])
     end
+
+    def new
+        @movie = Movie.new
+    end
+
+    def create
+        Movie.create(title: movie_params[:title], genre: movie_params[:genre], year: movie_params[:year], length: movie_params[:length])
+        redirect_to movies_path
+    end
+
+    def movie_params
+        params.require(:movie).permit(:title, :genre, :year, :length)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/movies", to: "movies#index"
-  get "/movies/:id", to: "movies#show"
-
+  get "/movies/new", to: "movies#new"
+  get "/movies/:id", to: "movies#show", as: "movie"
+  post "movies", to: "movies#create"
+  
   get "/actors", to: "actors#index"
 end

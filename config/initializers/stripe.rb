2807a67153792f0eaf27f98ad718edd20777b62f
Cameron Jones <iamcrjones@gmail.com>
2022-02-17 09:34:57 +1000

    key revert

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,5 +1,5 @@
 Rails.configuration.stripe = {
-    Stripe.setPublishableKey(:publishable_key => ENV['publishable_key']),
+    :publishable_key => ENV['publishable_key'],
     :secret_key      => ENV['secret_key']
  }
  Stripe.api_key = Rails.configuration.stripe[:secret_key]

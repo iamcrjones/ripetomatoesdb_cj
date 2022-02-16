f37ddf76514b08dc5f4821487f9c3689b3f6c54a
Cameron Jones <iamcrjones@gmail.com>
2022-02-17 09:29:16 +1000

    key change

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,5 +1,5 @@
 Rails.configuration.stripe = {
-    :publishable_key => ENV['publishable_key'],
+    Stripe.setPublishableKey(:publishable_key => ENV['publishable_key']),
     :secret_key      => ENV['secret_key']
  }
  Stripe.api_key = Rails.configuration.stripe[:secret_key]
